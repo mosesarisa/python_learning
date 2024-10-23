f7565d83b75d0d1b5f057640c345820eb168359e
mosesarisa <snazzy12309@gmail.com>
2024-10-23 14:43:55 +0300

    5.py

---
+++ 5.py
@@ -0,0 +1,11 @@
+likes = input("Does Nelly like you? (yes/no): ").strip().lower()
+hates = input("Does Nelly hate you? (yes/no): ").strip().lower()
+
+if likes == "yes":
+    print("I like you too!!!")
+    print("I'm looking forward to meeet you")
+elif hates == "yes":
+    print("Sorry for feeling that way :)")
+    print("Hey it's okay ")
+else:
+    print("MUCH LOVE !!!!")
