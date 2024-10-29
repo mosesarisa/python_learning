fcb010309a1b86f6b317e89dea6c32020f359ac2
mosesarisa <snazzy12309@gmail.com>
2024-10-29 11:55:27 +0300

    if.py

---
+++ 9.py
@@ -0,0 +1,11 @@
+hot_day = False
+cold_day = False
+
+if hot_day:
+    hot_day = True
+    print("Hey take a shower ")
+elif cold_day:
+    cold_day = True
+    print("Hey stay warm ")
+else:
+    print("It's another good day!!!")
