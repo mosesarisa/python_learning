0126910bc7c79f6b6e6bd1762dafe1b4ec7cbc1c
mosesarisa <snazzy12309@gmail.com>
2024-10-29 12:55:52 +0300

    3if.py

---
+++ 12.py
@@ -0,0 +1,13 @@
+temperature = int(input("enter temperature: "))
+
+if temperature > 37:
+    print("DANDER RUN !!!! ")
+elif temperature > 30:
+    print("It's a hot day")
+    print("wear light cloths")
+elif temperature < 20:
+    print("It's a cold day")
+    print("Wear warm cloths")
+
+else:
+    print("It's a another day !!!")
