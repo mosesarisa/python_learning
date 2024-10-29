e6027a4a3e3248784a44f4038376a00df17fc6fc
mosesarisa <snazzy12309@gmail.com>
2024-10-29 12:11:43 +0300

    2if.py

---
+++ 10.py
@@ -0,0 +1,10 @@
+House_price = 1000000
+has_good_credit = False
+
+if has_good_credit:
+    deposit = (House_price * 0.1)
+    
+else:
+    deposit = (House_price * 0.2)
+print(f"Deposit: ${deposit}")
+
