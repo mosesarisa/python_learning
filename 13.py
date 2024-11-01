fd395da157753dffc7c1ac164d94365debb6945d
mosesarisa <snazzy12309@gmail.com>
2024-11-01 13:17:55 +0300

    greet

---
+++ 13.py
@@ -0,0 +1,7 @@
+def greet(who="Colin"):
+    print("Hello,", who)
+    
+greet()
+greet(who="Kaggle")
+# (In this case, we don't need to specify the name of the argument, because it's unambiguous.)
+greet("world")
