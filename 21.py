8ad8f97780378ae3e260c4bb043d4bc352324e01
mosesarisa <snazzy12309@gmail.com>
2024-11-02 16:21:23 +0300

    21.py

---
+++ 21.py
@@ -0,0 +1,12 @@
+def num(x):
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+
+num(0)
+num(-15)
