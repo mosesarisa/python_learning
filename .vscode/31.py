0beca1f7aded19ef349b4a465e2f404cfddaf912
mosesarisa <snazzy12309@gmail.com>
2024-11-04 15:52:22 +0300

    31.py

--- .vscode/31.py
+++ .vscode/31.py
@@ -1,3 +1,5 @@
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 short_planets = [planet for planet in planets if len(planet) < 6]
+loud_short_planets = [planet.upper() + '!' for planet in planets if len(planet) < 6]
+print(loud_short_planets)
 print(short_planets)
