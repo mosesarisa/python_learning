dd2f0ca9d8d7d5e674756cfcb7c1449d00a38644
mosesarisa <snazzy12309@gmail.com>
2024-11-04 14:24:57 +0300

    26.py

---
+++ 26.py
@@ -0,0 +1,5 @@
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+msg = ''
+for char in s:
+    if char.isupper():
+        print(char, end='') 
